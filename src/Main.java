f45eba4d6ddcbf4c2d275c322f30440de22cd34f
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-30 12:43:31 +0530

    arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
-    }
-}
+
+
+        /**
+         * array list program
+         */
+                List<String> list=new ArrayList<String>();//Creating arraylist
+                list.add("abhi");//Adding object in arraylist
+                list.add("ankur");
+                list.add("ankit");
+                list.add("ravi");
+//Traversing list through Iterator
+                Iterator itr=list.iterator();
+                while(itr.hasNext()){
+                    System.out.println(itr.next());
+                }
+            }
+        }
+
+
